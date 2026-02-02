8ae4950c8afcd58050538b2d5875edc0ec019eb1
coder100605 <martinisedoardo@gmail.com>
2026-02-02 21:17:02 +0100

    Create Tipi-di-dati.py

---
+++ Tipi-di-dati.py
@@ -0,0 +1,16 @@
+x = 7 
+y = 3 
+print(x+y)
+
+prezzo = 19.99
+quantità = 3
+totale = prezzo * quantità
+print(totale)
+
+z = 10
+print( z > 5)
+
+a = 5
+b = 2.5
+
+print ("la somma di a e b è ",a+b)
